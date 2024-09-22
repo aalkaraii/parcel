e11e8d8964f3e5c607a225944b0f26c4f052bc86
Alka Rai <mrsalka02@gmail.com>
2024-09-22 13:57:20 +0545

    dashboard further designed

--- client/src/app/Dashboard/page.js
+++ client/src/app/Dashboard/page.js
@@ -1,17 +1,27 @@
 import React from "react";
 import LeftSideBar from "../Compoment/SideBar/LeftSideBar/page";
 import RightSideBar from "../Compoment/SideBar/RightSideBar/page";
+import CustomNavbar from "../Compoment/NavBar/page";
+import { Input } from "postcss";
 
 const Dashboard = () => {
   return (
     <div>
+      <div className="bg-blue-300 absolute top-0 left-0 right-0 z-10">
+        <CustomNavbar />
+      </div>
       <div class="flex min-h-screen">
         <main class="flex-grow p-2 w-1/2 bg-gray-100">
           <LeftSideBar />
         </main>
-        <aside class="w-1/4 bg-gray-300 p-4">
-          <RightSideBar />
-        </aside>
+
+        <div className="flex  items-center justify-between w-screen p-20 bg-blue">
+          <div />
+
+          <aside>
+            <RightSideBar />
+          </aside>
+        </div>
       </div>
     </div>
   );
