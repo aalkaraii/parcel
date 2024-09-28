0764d69bba6d832c65033eae5fc7e9e180810675
Alka Rai <mrsalka02@gmail.com>
2024-09-28 23:05:11 +0545

    circula progress added on dashboard

--- client/src/app/Dashboard/page.js
+++ client/src/app/Dashboard/page.js
@@ -1,26 +1,46 @@
 import CustomNavbar from "@/component/NavBar/page";
-import LeftSideBar from "@/component/SideBar/LeftSidebar/page";
-import RightSideBar from "@/component/SideBar/RightSidebar/page";
+import {
+  CircularProgress,
+  Card,
+  CardBody,
+  CardFooter,
+  Chip,
+} from "@nextui-org/react";
 import React from "react";
 
 const Dashboard = () => {
   return (
-    <div>
+    <div className="flex bg-white">
       <div className="bg-transparent absolute top-0 left-0 right-0 z-10">
         <CustomNavbar />
       </div>
-      <div class="flex min-h-screen">
-        <main class="flex-grow p-2 w-1/2 bg-gray-100">
-          <LeftSideBar />
-        </main>
-
-        <div className="flex  items-center justify-between w-screen p-20 bg-blue">
-          <div />
-
-          <aside>
-            <RightSideBar />
-          </aside>
-        </div>
+      <div className="bg-transparent min-w-full mt-20 p-10">
+        <Card className="w-[240px] h-[240px] border-none bg-gradient-to-br from-violet-500 to-fuchsia-500">
+          <CardBody className="justify-center items-center pb-0">
+            <CircularProgress
+              classNames={{
+                svg: "w-36 h-36 drop-shadow-md",
+                indicator: "stroke-white",
+                track: "stroke-white/10",
+                value: "text-3xl font-semibold text-white",
+              }}
+              value={70}
+              strokeWidth={4}
+              showValueLabel={true}
+            />
+          </CardBody>
+          <CardFooter className="justify-center items-center pt-0">
+            <Chip
+              classNames={{
+                base: "border-1 border-white/30",
+                content: "text-white/90 text-small font-semibold",
+              }}
+              variant="bordered"
+            >
+              2800 Data points
+            </Chip>
+          </CardFooter>
+        </Card>
       </div>
     </div>
   );
